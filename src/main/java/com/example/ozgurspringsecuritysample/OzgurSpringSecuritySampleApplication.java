6aba687392fe73e9e10faab7f475b9aa4998a165
Ozgur Demir <ozgur.d@freelancer.run>
2022-10-04 16:13:41 +0100

    Initial commit

---
+++ src/main/java/com/example/ozgurspringsecuritysample/OzgurSpringSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ozgurspringsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzgurSpringSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OzgurSpringSecuritySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ozgurspringsecuritysample/OzgurSpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ozgurspringsecuritysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzgurSpringSecuritySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
